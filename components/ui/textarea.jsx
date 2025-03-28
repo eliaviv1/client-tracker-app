0bb345a071e69d56174a286aff2742dfde15caa1
eliaviv1 <eliabit@gmail.com>
2025-03-28 16:34:33 +0300

    Add files via upload

---
+++ components/ui/textarea.jsx
@@ -0,0 +1,8 @@
+export function Textarea({ className = "", ...props }) {
+  return (
+    <textarea
+      className={`border border-gray-300 rounded px-3 py-2 w-full ${className}`}
+      {...props}
+    />
+  );
+}
